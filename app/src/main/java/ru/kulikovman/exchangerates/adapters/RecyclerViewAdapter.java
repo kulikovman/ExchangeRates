65e71fbe48575f92d0cef3c9cacfeeec729ffe71
Maksim Kulikov <kulikovman@gmail.com>
2018-06-26 21:02:21 +0500

    Готов адаптер для списка данных

--- app/src/main/java/ru/kulikovman/exchangerates/adapters/RecyclerViewAdapter.java
+++ app/src/main/java/ru/kulikovman/exchangerates/adapters/RecyclerViewAdapter.java
@@ -1,4 +1,79 @@
 package ru.kulikovman.exchangerates.adapters;
 
-public class RecyclerViewAdapter {
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import ru.kulikovman.exchangerates.R;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+    private List<Integer> mData;
+    private LayoutInflater mInflater;
+    private ItemClickListener mClickListener;
+
+    // data is passed into the constructor
+    RecyclerViewAdapter(Context context, List<Integer> data) {
+        this.mInflater = LayoutInflater.from(context);
+        this.mData = data;
+    }
+
+    // inflates the row layout from xml when needed
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = mInflater.inflate(R.layout.item_date, parent, false);
+        return new ViewHolder(view);
+    }
+
+    // binds the data to the TextView in each row
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        int animal = mData.get(position);
+        holder.mDateField.setText(animal);
+    }
+
+    // total number of rows
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+    // stores and recycles views as they are scrolled off screen
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        TextView mDateField;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            mDateField = itemView.findViewById(R.id.date_field);
+            mDateField.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            if (mClickListener != null) {
+                mClickListener.onItemClick(v, getAdapterPosition());
+            }
+        }
+    }
+
+    // convenience method for getting data at click position
+    int getItem(int id) {
+        return mData.get(id);
+    }
+
+    // allows clicks events to be caught
+    void setClickListener(ItemClickListener itemClickListener) {
+        this.mClickListener = itemClickListener;
+    }
+
+    // parent activity will implement this method to respond to click events
+    public interface ItemClickListener {
+        void onItemClick(View view, int position);
+    }
 }
