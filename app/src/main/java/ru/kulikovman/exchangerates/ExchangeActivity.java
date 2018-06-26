fe4e52344a7ae92177c3f5168e66fcda8a642c72
Maksim Kulikov <kulikovman@gmail.com>
2018-06-26 23:59:41 +0500

    Извлечение даты из интента

--- app/src/main/java/ru/kulikovman/exchangerates/ExchangeActivity.java
+++ app/src/main/java/ru/kulikovman/exchangerates/ExchangeActivity.java
@@ -5,11 +5,15 @@ import android.os.Bundle;
 import android.view.MenuItem;
 
 public class ExchangeActivity extends AppCompatActivity {
+    long mDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exchange);
+
+        mDate = (long) getIntent().getSerializableExtra("date_in_long");
+
     }
 
     @Override
